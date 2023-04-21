2f264566f07a505974ccc6bc93075c89f77a9b11
UE-Asuncion <asuncion.markanthony@ue.edu.ph>
2023-04-21 21:22:15 +0800

    --

--- app/src/main/java/com/aaronman/arrow/MainActivity.java
+++ app/src/main/java/com/aaronman/arrow/MainActivity.java
@@ -10,46 +10,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
-import org.xmlpull.v1.XmlPullParser;
-
 public class MainActivity extends AppCompatActivity {
-    FragmentManager fragmentManager = null;
+//    static{
+//
+//        if(OpenCVLoader.initDebug()){
+//
+//            Log.d(“Check”,”OpenCv configured successfully”);
+//
+//        } else{
+//
+//            Log.d(“Check”,”OpenCv doesn’t configured successfully”);
+//
+//        }
+//
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Fragment Manager
-        if (fragmentManager == null) {
-            fragmentManager = getSupportFragmentManager();
-        }
-        int id = fragmentManager.beginTransaction()
-                .replace(R.id.camera_container, Camera.class, null, "main_camera")
-                .setReorderingAllowed(true)
-                .addToBackStack(null)
-                .commit();
-        if (id > 0) { fragmentManager.popBackStack(); }
-        //overriding onBackPressed
-        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
-            @Override
-            public void handleOnBackPressed() {
-                finish();
-            }
-        };
-        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        ImageButton camButton = findViewById(R.id.camera_button);
-        camButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("Log","Open Cam");
-            }
-
-        });
-
     }
 }
