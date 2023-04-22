e80966364e84850b83108a61ca7463b08986c7ee
UE-Asuncion <asuncion.markanthony@ue.edu.ph>
2023-04-22 15:13:27 +0800

    --

--- app/src/main/java/com/aaronman/arrow/MainActivity.java
+++ app/src/main/java/com/aaronman/arrow/MainActivity.java
@@ -1,30 +1,25 @@
 package com.aaronman.arrow;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
-import org.opencv.*;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfDouble;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.ArucoDetector;
 
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -32,8 +27,11 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private static BaseLoaderCallback baseLoaderCallback;
     private int screen_w;
     private  int screen_h;
-    private Mat frame;
+    private Mat mRgba;
     private Mat gray;
+    private int img_id;
+    private Bitmap img;
+    private Mat mat_img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         // getting cam permission
         getPermission();
 
-        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.fd_activity_surface_view);
+        cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
         cameraBridgeViewBase.setCvCameraViewListener(this);
         cameraBridgeViewBase.setCameraPermissionGranted();
@@ -49,13 +47,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             @Override
             public void onManagerConnected(int status) {
                 Log.i("Arrow","onManagerCOnnected");
-                switch (status){
-                    case BaseLoaderCallback.SUCCESS:
-                        cameraBridgeViewBase.enableView();
-                        break;
-                        default:
-                            super.onManagerConnected(status);
-                            break;
+                if (status == BaseLoaderCallback.SUCCESS) {
+                    cameraBridgeViewBase.enableView();
+                }else{
+                    super.onManagerConnected(status);
                 }
             }
         };
@@ -82,6 +77,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         if(OpenCVLoader.initDebug()){
             Log.i("ArrowApp","initDebug");
             baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+            if (img == null){
+//                img_id = getResources().getIdentifier("ue_seal_09","raw",getPackageName());
+                img = BitmapFactory.decodeResource(getResources(),R.drawable.circle);
+            }
         }else Log.i("ArrowApp","intDebug Fails");
     }
 
@@ -114,18 +113,27 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         screen_w = width;
         screen_h = height;
         gray = new Mat(screen_w,screen_h, CvType.CV_8UC1);
-        frame = new Mat(screen_w,screen_h,CvType.CV_8SC4);
+        mRgba = new Mat(screen_w,screen_h,CvType.CV_8SC4);
     }
 
     @Override
     public void onCameraViewStopped() {
-        frame.release();
+        mRgba.release();
         gray.release();
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        frame = inputFrame.rgba();
-        return frame;
+        mRgba = inputFrame.rgba();
+        // process frame here
+
+        mRgba = inputFrame.rgba();
+        int w = mRgba.width();
+        int h = mRgba.height();
+
+        Imgproc.rectangle(mRgba, new Point(screen_w * 1 / 3, screen_h * 1 / 3), new Point(
+                screen_w * 2 / 3, screen_h * 2 /  3 ), new Scalar( 0, 255, 0 ), -1);
+
+        return mRgba;
     }
 }
